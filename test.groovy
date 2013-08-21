e65a3c73dd2a2b722875a985c016d09bee057d93
sarnobat <ss401533@gmail.com>
2013-08-20 19:27:20 -0700

    Initial commit

--- test.groovy
+++ test.groovy
@@ -1,9 +1,33 @@
+import org.neo4j.graphdb.*;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 import org.neo4j.tooling.GlobalGraphOperations;
 
 GraphDatabaseService graphDb = new GraphDatabaseFactory().newEmbeddedDatabase("yurl.db");
+
+
+//
+// Create node
+//
+
+Transaction tx = graphDb.beginTx();
+try
+{
+	Node firstNode = graphDb.createNode();
+	firstNode.setProperty( "url", "http://www.objectivity.com" );
+	// Updating operations go here
+	tx.success();
+}
+finally
+{
+	tx.finish();
+}
+
+
+//
+// Print nodes
+//
 
 Iterable<Node> allNodes = GlobalGraphOperations.at(graphDb).getAllNodes();
 for (final Node node : allNodes) {
@@ -12,8 +36,8 @@ for (final Node node : allNodes) {
 		title = (String) node.getProperty("title");
 	}
 	String name = "";
-	if (node.hasProperty("name")) {
-		name = (String) node.getProperty("name");
+	if (node.hasProperty("url")) {
+		name = (String) node.getProperty("url");
 	}
 
 	System.out.println("\"" + title + "\",\"" + name + "\"");
